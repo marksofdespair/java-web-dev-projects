e3b5c1b06341ecd0b8660efdada453c0d681303b
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-12-04 14:29:41 -0600

    Submission

---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/arraypractice.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class arraypractice {
+    public static void main(String[] args) {
+        // This CREATES AND INITIALIZES the array. Pls.
+        int[] arrayOne = {1, 1, 2, 3, 5, 8};
+
+        // Loops through array, printing ea value
+        for (int i = 0; i < arrayOne.length; i++) {
+            System.out.println(arrayOne[i]);
+        }
+    }
+}
